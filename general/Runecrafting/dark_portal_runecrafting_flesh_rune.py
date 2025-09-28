470cb60791c97a900def59ad0bbf41b7940b6bff
Semir Smajlovic <Semirxbih@gmail.com>
2025-09-28 07:06:02 -0500

    Update

--- general/Runecrafting/dark_portal_runecrafting_flesh_rune.py
+++ general/Runecrafting/dark_portal_runecrafting_flesh_rune.py
@@ -128,7 +128,7 @@ RUNECRAFTING_STEPS = [
     {
         'name': 'Click Reset Camera',
         'emoji': '📷',
-        'duration': (4.0, 6.0),
+        'duration': (3.0, 5.0),
         'region_key': 'RESET_CAMERA_REGION2'
     }
 ]
